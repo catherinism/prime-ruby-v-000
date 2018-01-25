a2f49c4d5565af66daaeb4090ad4227350e64c33
Catherine Salazar <catcreates13@gmail.com>
2018-01-25 01:07:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,13 +7,3 @@ def prime(integer)
   (2..integer/2).none? {|i| integer % i == 0}
 end
 end
-
-def prime? (n)
-    if n <= 1
-        false
-    elsif n == 2
-        true
-    else
-        (2..n/2).none? { |i| n % i == 0}
-    end
-end
